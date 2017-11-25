c530c1733fac369dd2757bb07f7a25c7ef5c70d6
miguelkjkszpj <miguel.torres2016@alu.uct.cl>
2017-11-25 19:37:20 -0300

    perfil
    agrege a la base pdi, en la tabla usuario, una columna llamada "imagen_usuario"

--- app/views/accessControl/perfil.php
+++ app/views/accessControl/perfil.php
@@ -1,14 +1,23 @@
 <?php
 	$i=0;
-	$nombresParaElPerfil=["Nombre","Apellido","Rut","Fecha de nacimiento","Correo","Password","Nivel","Id","Fecha de Nacimiento"];
+	$nombresParaElPerfil=["Nombre","Apellido","Rut","Fecha de nacimiento","Correo","Password","Nivel","Id","Fecha de Nacimiento",""];
 	if($user){
 	//el nombre es largo para no interrumpir a nadie XD ?>
-	<div class='col-xl-6 thumbnail'>
-		<?php foreach($user as $perfil){ if($perfil != ""){ ?>
-				<p><?= $nombresParaElPerfil[$i] ?> : <?= $perfil ?></p>
-			<?php }$i++;
+	<div class='col-xl-6 thumbnail row' style="background-color:rgba(99,185,255,0.5)">
+		<div class='col-md-8'>
+		<?php foreach($user as $perfil){ ?>
+			<?php if($nombresParaElPerfil[$i]!="" && $perfil != ""){ ?>
+				<h1 class="bg-primary"><?= $nombresParaElPerfil[$i] ?> : <?= $perfil ?></h1>
+			<?php }else{$imagen_perfil= $perfil ? $perfil : "no imagen";}
+			 $i++;
+			}
 	} ?>
 </div>
-<?php } else { ?>
-	<p>no estas registrado</p>
-<?php }?>
+<br><br><br><div class='col-md-4 thumbnail' id='palafoto'></div></div>
+<script> 
+	<?php echo"var fotoaponer = '$imagen_perfil';" ?>
+	fotoaponer = "<img src='"+fotoaponer+"'>";
+	var paeditarlafoto = document.getElementById('palafoto');
+	paeditarlafoto.innerHTML = fotoaponer;
+</script>
+<!-- no hay caso donde no este registrado, pues no aparece la opcion si no ingresa -->
