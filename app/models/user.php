38e877e8ec78b65e8f05c7930827b03b77f5b3f8
miguelkjkszpj <miguel.torres2016@alu.uct.cl>
2017-11-29 18:14:38 -0300

    parte 4

--- app/models/user.php
+++ app/models/user.php
@@ -11,12 +11,13 @@ class User extends Model{
     public $birthdate;
     public $email;
     public $password;
+    public $img;
 
     function create($data){
         $model = new static();
         $pass=password_hash( $data->password, PASSWORD_DEFAULT, array("cost"=>12));
 
-        $sql="INSERT INTO ".$model->table."( `nombre`, `apellido`, `rut`, `fecha de nacimiento`, `correo`, `password`, `nivel`) VALUES ( :name , :surname, :rut, :birthdate, :email, :password, 1 )";
+        $sql="INSERT INTO ".$model->table."( `nombre`, `apellido`, `rut`, `date`, `correo`, `password`, `nivel`) VALUES ( :name , :surname, :rut, :birthdate, :email, :password, 1 )";
 
         $params = [ ":name" => $data->nombres, ":surname" => $data->apellido, ":rut" => $data->rut, ":birthdate" => $data->birthdate, ":email" => $data->email, ":password" => $pass ];
 
--- app/views/accessControl/perfil.php
+++ app/views/accessControl/perfil.php
@@ -1,24 +1,46 @@
 <?php
 	$i=0;
-	$nombresParaElPerfil=["Nombre","Apellido","Rut","Fecha de nacimiento","Correo","Password","Nivel","Id","Fecha de Nacimiento",""];
 	if($user){
 	//el nombre es largo para no interrumpir a nadie XD ?>
-	<div class='col-xl-6 thumbnail row' style="background-color:rgba(0,182,255,0.6)">
+	<div class='col-xl-10 thumbnail row' style="background-color:rgba(240,240,255,0.9)">
+		<br> <br> <br> <div class='col-md-4 thumbnail' id='palafoto'></div>
+		<button onclick="location.href = '<?php echo SUB_FOLDER; ?>/profile/EditP'" action="EditP" id="staid" type="button" class="btn btn-primary" >cambiar datos</button>
 		<div class='col-md-8'>
-		<?php foreach($user as $perfil){ ?>
-			<?php if($nombresParaElPerfil[$i]!="" && $perfil != "" && $nombresParaElPerfil[$i] !="Password"){ ?>
-				<h1 class="text-muted"><?= $nombresParaElPerfil[$i] ?> : <?= $perfil ?></h1>
-			<?php }else{$imagen_perfil= $perfil ? $perfil : SUB_FOLDER."/img/user-image-not-available.png";}
-			 $i++;
-			}
-	} ?>
-	<button onclick="location.href = '<?php echo SUB_FOLDER; ?>/profile/EditP'" action="EditP" id="staid" type="button" class="btn btn-primary" >cambiar datos</button>
+			<table border="1 px">
+				<caption>Datos</caption>
+				<tbody>
+					<tr>
+						<td>Nombre:</td>
+						<td><?= $user->nombre ?></td>
+					</tr>
+					<tr>
+						<td>Apellido:</td>
+						<td><?= $user->apellido ?></td>
+					</tr>
+					<tr>
+						<td>Rut:</td>
+						<td><?= $user->rut ?></td>
+					</tr>
+					<tr>
+						<td>Fecha de nacimiento:</td>
+						<td><?= $user->date ?></td>
+					</tr>
+					<tr>
+						<td>Correo:</td>
+						<td><?= $user->correo ?></td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
-<br> <br> <br> <div class='col-md-4 thumbnail' id='palafoto'></div> </div>
+		</div>
+ </div>
+ <?php $imagen_perfil = $user->img ? $user->img : SUB_FOLDER."/img/user-image-not-available.png"
+  //$imagen_perfil = SUB_FOLDER."/img/user-image-not-available.png"?>
 <script> 
 	<?php echo"var fotoaponer = '$imagen_perfil';" ?>
 	fotoaponer = "<img src='"+fotoaponer+"'>";
 	var paeditarlafoto = document.getElementById('palafoto');
 	paeditarlafoto.innerHTML = fotoaponer;
 </script>
+<?php } ?>
 <!-- no hay caso donde no este registrado, pues no aparece la opcion si no ingresa -->
